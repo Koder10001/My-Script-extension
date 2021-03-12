a26356814064a440e0df977a03728db681b1ab8a
koder10001 <koder10001@gmail.com>
2021-03-12 01:26:12 -0600

    aaa

--- background.js
+++ background.js
@@ -4,6 +4,9 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
                 "from the extension");
     if (request.download)
     {
+        chrome.cookies.get(sender.tab.url,(cookie)=>{
+            console.log(cookie);
+        })
         chrome.downloads.download({
             url: request.download
         });
